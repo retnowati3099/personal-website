96fc05262e283b0eaee6ee2ec45b8f32248ba68f
Retno wati <retnowati3099@gmail.com>
2023-10-29 21:06:05 +0700

    made main content

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import About from './About'
+import Skills from './Skills'
+import Portfolio from './Portfolio'
+import Certifications from './Certifications'
+import Contacts from './Contacts'
+
+function Main() {
+  return (
+    <main>
+        <About />
+        <Skills />
+        <Portfolio />
+        <Certifications />
+        <Contacts />
+    </main>
+  )
+}
+
+export default Main
