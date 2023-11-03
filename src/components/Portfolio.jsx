85d151a82ec6329267c852352bb84f1fd72889eb
Retno wati <retnowati3099@gmail.com>
2023-11-03 08:33:48 +0700

    added id into portfolio session

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <div id='portfolio' className='flex flex-col items-center p-[81px] gap-[72px]'>
+      <div className='flex flex-col gap-3'>
+        <span className='text-[#27374D] text-[32px] text-center font-bold'>Portfolio</span>
+        <span className='text-[#353535] text-xl text-center'>Those are some of my projects</span>
+      </div>
+      <div className='flex justify-between w-[1118px] p-10 bg-[#DDE6ED] rounded-lg'>
+        <div className='flex-1'> 
+          {/* text */}
+          <div className='flex flex-col gap-4'>
+            <span>Website of Sukarara and Bonjeruk
+              Tourist Villages
+            </span>
+            <span>A website that is used to introduce Sukarara and Bonjeruk tourist villages</span>
+            <span>Stack: HTML 5, CSS 3, JavaScript</span>
+          </div>
+          {/* button */}
+          <div></div>
+        </div>
+        <div className='flex-1'>
+          <img src="/src/assets/hikayatjonggat-desktop.png" alt="website hikayatjonggat" />
+        </div>
+      </div>
+    </div>
   )
 }
 
