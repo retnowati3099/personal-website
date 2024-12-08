c27eb89797a475bbba9d8f8e211e5f34a7edef89
retnowati3099 <retnowati3099@gmail.com>
2024-12-08 17:40:39 +0700

    making responsive in the home and footer section

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ function showSlide(n) {
     }
     slide[slideIndex - 1].style.display = "block";
 }
+
+let isDarkMode = false;
+
+function changeMode() {
+    isDarkMode = !isDarkMode;
+    let body = document.querySelector("body")
+    let mode = document.querySelector("#mode")
+    let header = document.querySelector("header")
+    if (isDarkMode) {
+        body.style.backgroundColor = "#353535";
+        body.style.color = "white";
+        mode.src = "assets/light_mode.svg";
+        header.style.backgroundColor = "#353535";
+    } else {
+        body.style.backgroundColor = "white";
+        mode.src = "assets/dark_mode.svg";
+        header.style.backgroundColor = "white";
+    }
+}
